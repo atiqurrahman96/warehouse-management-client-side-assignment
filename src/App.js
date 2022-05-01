c67f05e26b83bc345a103ea477ae58006f8d1741
atiqurrahman96 <atiqurrahmanbd302@gmail.com>
2022-05-01 13:15:02 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Blogs from './Pages/Blogs/Blogs';
+import Header from './Pages/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
 
+        <Route path='/blogs' element={<Blogs />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+            <Container>
+                <Navbar.Brand href="home#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="home#home">Home</Nav.Link>
+
+
+                    </Nav>
+                    <Nav>
+                        {/* <Nav.Link as={Link} to='/login'>Log in</Nav.Link> */}
+
+                        <Nav.Link as={Link} to='/blogs'>Blogs</Nav.Link>
+                        <Nav.Link as={Link} to='/login'>Log in</Nav.Link>
+
+                        <Nav.Link eventKey={2} href="#signUp">
+                            Sign Up
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
