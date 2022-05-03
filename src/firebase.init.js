2dea21e096b3395c4dda7ffe7737228fac2fd7a8
atiqurrahman96 <atiqurrahmanbd302@gmail.com>
2022-05-03 23:42:28 +0600

    .env local file added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA4Rp2q6n76u9fN4VgVdyIt-UM1oztQgmc",
-    authDomain: "my-grocery-warehouse-services.firebaseapp.com",
-    projectId: "my-grocery-warehouse-services",
-    storageBucket: "my-grocery-warehouse-services.appspot.com",
-    messagingSenderId: "307311324724",
-    appId: "1:307311324724:web:464256ccf9748b5ee3a461"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
