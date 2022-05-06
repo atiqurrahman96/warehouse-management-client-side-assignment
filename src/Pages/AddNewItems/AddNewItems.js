a0bfeaa69fb4d88f7a6011a55881af634f6e3541
atiqurrahman96 <atiqurrahmanbd302@gmail.com>
2022-05-06 21:06:34 +0600

    manage inventories and deleted opton added

--- src/Pages/AddNewItems/AddNewItems.js
+++ src/Pages/AddNewItems/AddNewItems.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
-
-
 const AddNewItems = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data)
 
+        const url = `http://localhost:5000/products`
+        fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => response.json())
+            .then(result => {
+                console.log(result);
+            })
+    };
     return (
         <div className='w-50 mx-auto'>
             <h1>Welcome and add a new product</h1>
-            <form>
-                <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
-                    <input style={{ border: '1px solid black' }} className='mb-2' placeholder='name' {...register("firstName", { required: true, maxLength: 20 })} />
-                    <textarea style={{ border: '1px solid black' }} className='mb-2' placeholder='description' {...register("description")} />
-                    <input style={{ border: '1px solid black' }} className='mb-2' placeholder='photo Url' {...register("photo")} />
+            <form className='d-flex flex-column ' onSubmit={handleSubmit(onSubmit)}>
+                <input style={{ border: "1px solid black" }} className='mb-2' placeholder='Product Name' {...register("firstName", { required: true, maxLength: 20 })} />
 
-                    <input className='bg-primary w-25 mx-auto p-2 rounded' type="submit" value="submit" />
-                </form>
+                <textarea style={{ border: "1px solid black" }} className='mb-2' placeholder='Description'  {...register("description")} />
+                <input style={{ border: "1px solid black" }} className='mb-2' type="text" placeholder='photoURL'  {...register("image")} />
+                <input className='mb-2' placeholder='Quantity' type="number" {...register("quantity")} />
+                <input className='mb-2' placeholder='Price' type="number" {...register("price")} />
+                <input className='bg-primary w-50 mx-auto p-2 rounded text-white' type="submit" value="Add new Product" />
             </form>
-
-
         </div>
     );
 };
--- src/Pages/ManageInventories/ManageInventories.js
+++ src/Pages/ManageInventories/ManageInventories.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import ManageInventory from '../ManageInventory/ManageInventory';
+
 
 const ManageInventories = () => {
     const navigate = useNavigate();
@@ -14,18 +14,46 @@ const ManageInventories = () => {
             .then(res => res.json())
             .then(data => setInventories(data))
     }, [])
+    const deleteItem = id => {
+        const proceed = window.confirm('Are You permit to Delete')
+        if (proceed) {
+            const url = `http://localhost:5000/products/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log('successfully', data);
+                    const restProducts = inventories.filter(inventory => inventory._id !== id);
+                    setInventories(restProducts)
+                })
+        }
+    }
     return (
         <div>
             <h1>Manage inventories: {inventories.length}</h1>
             <div className='manage-inventories-items-container'>
                 {
-                    inventories.map(inventory => <ManageInventory
-                        inventory={inventory}
-                    ></ManageInventory>)
+                    inventories.map(inventory => <div key={inventory._id}>
+                        <div className='manage-inventory-container'>
+                            <div className='manage-inventory-img-container'>
+                                <img src={inventory.image} alt="" />
+                            </div>
+                            <div className='manage-inventory-detail-container'>
+                                <h3>Product:{inventory.name}</h3>
+                                <h4>Supplier:{inventory.supplier}</h4>
+                                <p>price:${inventory.price}</p>
+                                <p>Quantity:{inventory.quantity}</p>
+                                <p>Description:{inventory.description}</p>
+
+                            </div>
+                            <Button onClick={() => deleteItem(inventory._id)}>Delete</Button>
+                        </div>
+                    </div>)
                 }
 
             </div>
-            <Button onClick={addNewItem}>Add New Product</Button>
+            <Button className='mb-2' onClick={addNewItem}>Add New Product</Button>
         </div>
     );
 };
