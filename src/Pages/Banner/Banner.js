c0a4cf7965ae42679275c02dc6679392e0c01f1b
atiqurrahman96 <atiqurrahmanbd302@gmail.com>
2022-05-07 18:36:55 +0600

    banner added

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-
+import banner1 from '../../images/banner1 (3).jpg'
+import banner2 from '../../images/banner2 (2).jpg'
+import banner3 from '../../images/banner3 (2).jpg'
 const Banner = () => {
     return (
         <div>
@@ -8,35 +10,26 @@ const Banner = () => {
                 <Carousel.Item interval={1000}>
                     <img
                         className="d-block w-100"
-                        src="holder.js/800x400?text=First slide&bg=373940"
+                        src={banner1}
                         alt="First slide"
                     />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
+
                 </Carousel.Item>
                 <Carousel.Item interval={500}>
                     <img
                         className="d-block w-100"
-                        src="holder.js/800x400?text=Second slide&bg=282c34"
+                        src={banner2}
                         alt="Second slide"
                     />
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
+
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="holder.js/800x400?text=Third slide&bg=20232a"
+                        src={banner3}
                         alt="Third slide"
                     />
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
+
                 </Carousel.Item>
             </Carousel>
         </div>
